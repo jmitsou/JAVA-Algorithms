537d9c69c8964565170c402d927192cc2e393c19
Jeff Mitsou <shinetachi88@gmail.com>
2021-11-04 21:54:22 -0400

    new warm-ups

---
+++ src/com/company/Capture_the_Rook.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Capture_the_Rook {
+
+    public static boolean canCapture(String[] rooks) {
+
+    }
+}
---
+++ src/com/company/Largest_Gap.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Largest_Gap {
+
+    public static int largestGap(int[] numbers) {
+        int lGap = 0;
+
+        return lGap;
+    }
+}
